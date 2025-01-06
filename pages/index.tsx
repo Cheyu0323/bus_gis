de2581f0b12564be0bedb40522330f4551cce7cb
cheyu.chang <cheyu.chang@nadisystem.com>
2025-01-06 17:48:24 +0800

    feat: next speedInsights

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import dynamic from "next/dynamic";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const CesiumView = dynamic(() => import("@/components/CesiumView"), {
     ssr: false,
@@ -18,6 +19,7 @@ const Home = () => {
             <title>BUS GIS</title>
             <Analytics />
             <GoogleAnalytics gaId="G-F5R2FW96G7" />
+            <SpeedInsights />
             <section className="h-svh w-full relative">
                 <Suspense>
                     <CesiumView />
